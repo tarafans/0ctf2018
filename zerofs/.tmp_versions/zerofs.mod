3ee13a161fe3817ae158f51f27c1c6e6df03c6b3
tarafans <tarafans7@gmail.com>
2018-03-27 09:41:44 -0400

    x

--- zerofs/.tmp_versions/zerofs.mod
+++
@@ -1,3 +0,0 @@
-/home/test/0ctf2018/zerofs/zerofs.ko
-/home/test/0ctf2018/zerofs/zerofs.o
-
--- zerofs/.zerofs.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/test/0ctf2018/zerofs/zerofs.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/test/0ctf2018/zerofs/zerofs.ko /home/test/0ctf2018/zerofs/zerofs.o /home/test/0ctf2018/zerofs/zerofs.mod.o ;  true
